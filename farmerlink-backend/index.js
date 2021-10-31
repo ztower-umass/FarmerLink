924c159cb62d97571f111c3821aa444204c37014
sid2033 <siddharthraju299@gmail.com>
2021-10-31 15:19:28 -0400

    Sample API call and CORS added

--- farmerlink-backend/index.js
+++ farmerlink-backend/index.js
@@ -1,14 +1,16 @@
 //Template - Sid
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const faker = require("faker");
 const app = express();
 const port = 3000;
 const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-    "Access-Control-Max-Age": 2592000, // 30 days
-    /** add other headers too */
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    //Add Needed Headers
+
 };
+
+app.use(cors());
 
 //Try seding a request to http://localhost:3000/ to test if app is working
 app.get('/', (req, res) => {
@@ -18,9 +20,14 @@ app.get('/', (req, res) => {
 
 //For URL formats, use "/nameOfPage/details"
 //Make sure to set the headers before sending the response
-app.get('/forum/getCommentData', (req, res) => {
+app.get('/forum/getSampleCommentData', (req, res) => {
   res.set(headers);
-  res.send('Sample Comment Data');
+  let respJSON = {"commentTitle": "", "user": "", "upvotes": 0, "downvotes": 0}
+  respJSON.commentTitle = faker.lorem.sentences();
+  respJSON.user = faker.internet.userName();
+  respJSON.upvotes = Math.floor(Math.random() * 100);
+  respJSON.downvotes = Math.floor(Math.random() * 100);
+  res.send(JSON.stringify(respJSON));
 })
 
 app.listen(port, () =>
