a7292bdc00ce90de59c4e22559135d0b9279955e
Vidya Gopal <vidyagopal99@gmail.com>
2021-11-15 14:18:58 -0500

    updates package.json for ps dependency, added poolto connect to postgres in index.js

--- farmerlink-backend/index.js
+++ farmerlink-backend/index.js
@@ -1,3 +1,12 @@
+// Vidya changes for database
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 //Template - Sid
 const express = require("express");
 const cors = require("cors");
