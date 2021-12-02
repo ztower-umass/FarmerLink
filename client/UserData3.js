14628ae78542f5b9264c33ef367a131ae3fe56a2
Vidya Gopal <vidyagopal99@gmail.com>
2021-12-01 20:33:50 -0500

    remove unnecessary console logs

--- client/UserData3.js
+++ client/UserData3.js
@@ -177,8 +177,8 @@ async function getUser() {
 		
 		let resp = await getUsersdb(inData);
 		let data = resp.results;
-		console.log("Here ->" + JSON.stringify(resp) + "  " + data.length);
-		console.log("Here2 ->" + JSON.stringify(data));
+		//console.log("Here ->" + JSON.stringify(resp) + "  " + data.length);
+		//console.log("Here2 ->" + JSON.stringify(data));
 		console.log("Here3 ->" + resp.message);
 		//set values retrieved from database.
 		if (resp.message == 'Success') {
